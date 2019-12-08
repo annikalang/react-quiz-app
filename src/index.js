4e2d47a4c5a069db05c2d156b90db72d100ce6be
Annika Lang <Lang-annika@t-online.de>
2019-12-08 21:27:32 +0000

    creating QuizBee component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import React, { Component } from "react";
 
 // importing stylesheet
 import "./assets/style.css";
+
+// creating component called Quizbee, which extends the component class
+class QuizBee extends Component {
+  render () { // render method will return a JSX template, which renders on the page
+    return ();
+  }
+}
