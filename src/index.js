e60c7bb7c28318bacf013e225fc57570d18310a5
Annika Lang <Lang-annika@t-online.de>
2019-12-08 21:34:18 +0000

    importing React-DOM library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // whenever building a React-Component, the React library has to be imported first
 import React, { Component } from "react";
 
+// importing ReactDOM-Library to render our application on a web page
+import ReactDOM from "react-dom";
+
 // importing stylesheet
 import "./assets/style.css";
 
