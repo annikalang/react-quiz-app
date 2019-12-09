9a13cb63cf2e8d8c2444bc7c1693e10f2d1ece8b
Annika Lang <Lang-annika@t-online.de>
2019-12-09 23:25:01 +0000

    added componentDidMount() which is running the getQuestions function when this component loads up

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ import quizService from "./quizService/";
 
 // creating component called Quizbee, which extends the component class
 class QuizBee extends Component {
+
   state = { // instantiating local state for this component with an array named questionBank (state should always be located at the nearest parent)
     questionBank: [] // this ist where our five questions would be stored once we pull them in from the API
   };
+
   getQuestions = () => { // function that invokes the quizService API and proceeds to pupulate the questionBank state variable with the results
     quizService().then(question => {
       this.setState({ // used to update the state variable (the only way it should be done, never write to a state variable directly)
@@ -22,6 +24,11 @@ class QuizBee extends Component {
       });
     });
   };
+
+  componentDidMount() { // running getQuestions function when this component loads up
+    this.getQuestions();
+  }
+
   render () { // render method will return a JSX template, which renders on the page
     return (
       <div className="container">
