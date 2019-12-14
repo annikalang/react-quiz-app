013d84f0900b70d4a7eb74c849cee5dc2963048e
Annika Lang <Lang-annika@t-online.de>
2019-12-14 15:28:21 +0100

    to intercept clicks on these buttons, we use the onClick event listener property which will run a function. Inside this function we'll run the setAnswer method that we declared using the useState hook and we'll rewrite the answer state array with a new array containing only one element, which is the answer chosen by a click.

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -8,10 +8,13 @@ const QuestionBox = ({ question, options }) => {
   const [answer, setAnswer] = useState(options); // the setAnswer function refers to a function that can be used to update the value of the answer variable
   return (
     // using the map function on the answer variable to render a series of buttons, using the value of the index from the answer array as the key attribute (not ideal but works for this simple example, ideally unique values which will never mutate should be used).
+    // to intercept clicks on these buttons, we use the onClick event listener property which will run a function. Inside this function we'll run the setAnswer method that we declared using the useState hook and we'll rewrite the answer state array with a new array containing only one element, which is the answer chosen by a click.
     <div className="questionBox">
       <div className="question">{question}</div>
       {answer.map((text, index) => (
-        <button key={index} className="answerBtn">
+        <button key={index} className="answerBtn" onClick={() => {
+            setAnswer([text])
+          }}>
           {text}
         </button>
       ))}
