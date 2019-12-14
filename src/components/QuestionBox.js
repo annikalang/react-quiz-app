557628c79804ce5f61983dbcc2b101ec741fba82
Annika Lang <Lang-annika@t-online.de>
2019-12-14 15:39:33 +0100

    to compute the user's response and score, we need to pass the user's response from the QuestionBox component to the QuizBee component. So we need to add a new prop: selected (a function), ran when the user presses a button and we'll pass in the chosen answer

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -2,8 +2,9 @@
 // importing the useState method which will allow me to incorporate state in a function component (Hooks API)
 import React, { useState } from "react";
 
-// we don't create a class, instead we creatd a regular JS function
-const QuestionBox = ({ question, options }) => {
+// we don't create a class, instead we created a regular JS function
+// to compute the user's response and score, we need to pass the user's response from the QuestionBox component to the QuizBee component. So we need to add a new prop: selected (a function), ran when the user presses a button and we'll pass in the chosen answer
+const QuestionBox = ({ question, options, selected }) => {
   // creating a state variable names answer using the useState function and setting the initial value of the answer variable from the options array that we're getting here as a prop(erty)
   const [answer, setAnswer] = useState(options); // the setAnswer function refers to a function that can be used to update the value of the answer variable
   return (
@@ -16,7 +17,8 @@ const QuestionBox = ({ question, options }) => {
           key={index}
           className="answerBtn"
           onClick={() => {
-            setAnswer([text])
+            setAnswer([text]);
+            selected(text);
           }}
         >
           {text}
