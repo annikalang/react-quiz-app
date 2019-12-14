27c32ccc25696a39172e183da9d102ea7fa8cc91
Annika Lang <Lang-annika@t-online.de>
2019-12-14 15:16:07 +0100

    added further comments to explain setAnswer function

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 // we don't create a class, instead we creatd a regular JS function
 const QuestionBox = ({ question, options }) => {
   // creating a state variable names answer using the useState function and setting the initial value of the answer variable from the options array that we're getting here as a prop(erty)
-  const [answer, setAnswer] = useState(options);
+  const [answer, setAnswer] = useState(options); // the setAnswer function refers to a function that can be used to update the value of the answer variable
   return (
     <div className="questionBox">
       <div className="question">{question}</div>
