c9dd24a9be052d0d706c5363ca0d56b14136da41
Annika Lang <Lang-annika@t-online.de>
2019-12-14 14:54:01 +0100

    fixed typo, created QuestionBox function, exported function components QuestionBox

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,2 +1,14 @@
-// importing React library, as it must always be in the scope from creating components
+// importing React library, as it must always be in the scope when creating components
 import React from "react";
+
+// we don't create a class, instead we creatd a regular JS function
+const QuestionBox = ({ question, options }) => {
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
+    </div>
+  );
+};
+
+// export function components
+export default QuestionBox;
