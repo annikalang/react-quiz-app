92cc46eaba4daca9b26f874fe30fb0ad9d2770df
Annika Lang <Lang-annika@t-online.de>
2019-12-14 15:29:51 +0100

    reformatting for readability

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -12,9 +12,13 @@ const QuestionBox = ({ question, options }) => {
     <div className="questionBox">
       <div className="question">{question}</div>
       {answer.map((text, index) => (
-        <button key={index} className="answerBtn" onClick={() => {
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
             setAnswer([text])
-          }}>
+          }}
+        >
           {text}
         </button>
       ))}
