504637b468981b84a0cbe4e2a90aab73db1c39a2
Annika Lang <Lang-annika@t-online.de>
2019-12-15 23:06:41 +0100

    moved comments for better readability

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,11 +6,13 @@ import React from "react";
 // creating function component called result, as props, we'lls bring in the score and the function playAgain.
 const Result = ({score, playAgain}) => (
   // create a div with the className score-board, inside of which, we'll create another div.
+  // the other div to output the score
+  // building a button to invoke the contents of the playAgain prop as a function
   <div className="score-board">
-    // the other div to output the score
     <div className="score">You scored {score} / 5 correct answers!</div>
-    // building a button to invoke the contents of the playAgain prop as a function
-    <button className="playBtn" onClick={playAgain}>Play again!</button>
+    <button className="playBtn" onClick={playAgain}>
+      Play again!
+    </button>
   </div>
 )
 
